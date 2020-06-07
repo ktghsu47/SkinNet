a6857b239852907d8003c103d2e08f2c6e592e73
ktghsu47 <ktghsu@hotmail.com>
2020-06-07 19:43:19 -0400

    initial commit

---
+++ Core/Entities/Identity/AppUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Core.Entities.Identity {
+    public class AppUser : IdentityUser {
+        public string DisplayName { get; set; }
+        public Address Address { get; set; }
+    }
+}
